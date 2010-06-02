cda0545ad825db02ec2a2cb779c7dd1cb2d9b1b9
Joseph Ridgway <joseph@comsoft155.com>
2010-06-02 15:42:20 -0400

    Added staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,34 @@
+# Settings specified here will take precedence over those in config/environment.rb
+
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
+
+# Use a different logger for distributed setups
+# config.logger        = SyslogLogger.new
+
+
+# Full error reports are disabled and caching is on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
+config.action_controller.page_cache_directory = "#{RAILS_ROOT}/tmp/cache"
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host                  = "http://assets.example.com"
+
+# Disable delivery errors if you bad email addresses should just be ignored
+# config.action_mailer.raise_delivery_errors = false
+
+# Cache your content for a longer time, the default is 5.minutes
+# config.after_initialize do 
+#   SiteController.cache_timeout = 12.hours
+# end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN'],
+}
